04303fe23171732879f214f0debb3ea5997bbb6e
Mathias Johannes Hansen <majohha@gmail.com>
2022-08-10 15:56:07 -0500

    minor chrome adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative h-screen w-screen bg-gradient-to-b from-white to-slate-200 p-4 text-slate-800"
+    class="h-screen w-screen bg-gradient-to-b from-white to-slate-200 p-4 text-slate-800"
   >
     <suspense>
       <router-view></router-view>
